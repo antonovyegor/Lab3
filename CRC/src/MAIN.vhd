c7cca3d2d15b872b024b67b3498a01a6fb8343b9
antonovyegor <antonov.egor111@gmail.com>
2019-04-01 14:23:38 +0300

    update

--- CRC/src/MAIN.vhd
+++ CRC/src/MAIN.vhd
@@ -15,9 +15,8 @@ architecture BEH of CRC is
 	signal R : std_logic_vector(15 downto 0):= X"0000";
 	type STATE is (st1,st2,st3);
 	signal st : STATE := st1;
-	--signal shl_signal : std_logic;
-	--signal xor_signal : std_logic;
 	signal COUNT : integer :=0;
+	signal MAX : integer :=32+16;
 	signal M : std_logic_vector(0 to 47):=X"9B0E2C670000"  ;
 	begin
 
@@ -30,12 +29,13 @@ architecture BEH of CRC is
 				R(15)<=R(15) xor R(14);
 				R(2)<=R(1) xor R(15);
 				R(0)<= R(15) xor M(COUNT);
+				--R(0)<= R(15) xor INPUT;
 				COUNT<=COUNT+1;
 			end if;
 			if st=st2 then
-				M(47-15 to 47)<=R;
+				M(MAX-16 to MAX-1)<=R;
 				COUNT<=0;
-				M(45)<= not M(45);
+				M(MAX-4)<= not M(MAX-4);
 				R<=X"0000";
 			end if;
 
@@ -49,7 +49,7 @@ BEGIN
 	if C'event  then
 	CASE st IS
 		WHEN st1 =>
-		if COUNT = 48 then
+		if COUNT = MAX then
 			st<=st2;
 		else st<=st1;
 			end if;
