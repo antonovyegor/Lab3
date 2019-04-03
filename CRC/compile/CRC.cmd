0f497a15982a0b533dff7ea38b6bfe6deb47887d
antonovyegor <antonov.egor111@gmail.com>
2019-04-03 21:05:44 +0300

    backup

--- CRC/compile/CRC.cmd
+++ CRC/compile/CRC.cmd
@@ -1,1 +1,1 @@
--w -s "d:\My_Designs\Lab3\library.cfg"  -j "d:\My_Designs\Lab3\CRC\projlib.cfg"  -d "d:\My_Designs\Lab3\CRC\compile"   -work CRC  -O3 -e 100 -2002 "d:/My_Designs/Lab3/CRC/src/MAIN.vhd"
+-w -s "e:\My_Designs\Lab3\library.cfg"  -j "e:\My_Designs\Lab3\CRC\projlib.cfg"  -d "e:\My_Designs\Lab3\CRC\compile"   -work CRC  -O3 -e 100 -2002 "e:/My_Designs/Lab3/CRC/src/MAIN.vhd"
--- CRC/src/MAIN.vhd
+++ CRC/src/MAIN.vhd
@@ -15,11 +15,11 @@ architecture BEH of CRC is
 
 	type STATE is (st1,st2,st3);
 	signal st : STATE := st1;
-	signal N : integer :=8;
+	signal N : integer :=16;
 	signal COUNT : integer :=0;
 	signal MAX : integer :=32+N;
-	signal R : std_logic_vector(N-1 downto 0):= X"00";
-	signal M : std_logic_vector(0 to 32):=X"5F0E3C1700"  ;
+	signal R : std_logic_vector(N-1 downto 0):= conv_std_logic_vector(0,N);
+	signal M : std_logic_vector(0 to 47):=X"9B0E2C670000"  ;
 	begin
 
 	CRC: process (C)
@@ -27,21 +27,10 @@ architecture BEH of CRC is
 	begin
 		if C'event and C='1' then
 			if st=st1 then
-				--R(15 downto 1)<=R(N-2 downto 0);
-				--R(15)<=R(15) xor R(14);
-				--R(2)<=R(1) xor R(15);
-				--R(0)<= R(15) xor M(COUNT);
-
-
-				R(7)<=R(7) xor R(6);
-				R(6)<=R(7) xor R(5);
-				R(5)<=R(7) xor R(4);
-				R(4)<=R(7) xor R(3);
-				R(3)<=R(7) xor R(2);
-				R(2)<=R(1);
-				R(1)<=R(7) xor R(0);
-				R(0)<= R(7) xor M(COUNT);
-
+				R(15 downto 1)<=R(N-2 downto 0);
+				R(15)<=R(15) xor R(14);
+				R(2)<=R(1) xor R(15);
+				R(0)<= R(15) xor M(COUNT);
 
 				--R(0)<= R(15) xor INPUT;
 				COUNT<=COUNT+1;
@@ -49,8 +38,10 @@ architecture BEH of CRC is
 			if st=st2 then
 				M(MAX-N to MAX-1)<=R;
 				COUNT<=0;
+
 				M(MAX-4)<= not M(MAX-4);
-				R<=X"00";
+
+				R<=X"0000";
 			end if;
 
 		end if;
