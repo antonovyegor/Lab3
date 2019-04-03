f457f5c6658fe7d71a0e374798dcd695752f8485
antonovyegor <antonov.egor111@gmail.com>
2019-04-03 21:00:43 +0300

    для Леши

--- CRC/src/MAIN.vhd
+++ CRC/src/MAIN.vhd
@@ -12,12 +12,14 @@ entity CRC is
 end entity;
 
 architecture BEH of CRC is
-	signal R : std_logic_vector(15 downto 0):= X"0000";
+
 	type STATE is (st1,st2,st3);
 	signal st : STATE := st1;
+	signal N : integer :=8;
 	signal COUNT : integer :=0;
-	signal MAX : integer :=32+16;
-	signal M : std_logic_vector(0 to 47):=X"9B0E2C670000"  ;
+	signal MAX : integer :=32+N;
+	signal R : std_logic_vector(N-1 downto 0):= X"00";
+	signal M : std_logic_vector(0 to 32):=X"5F0E3C1700"  ;
 	begin
 
 	CRC: process (C)
@@ -25,18 +27,30 @@ architecture BEH of CRC is
 	begin
 		if C'event and C='1' then
 			if st=st1 then
-				R(15 downto 1)<=R(14 downto 0);
-				R(15)<=R(15) xor R(14);
-				R(2)<=R(1) xor R(15);
-				R(0)<= R(15) xor M(COUNT);
+				--R(15 downto 1)<=R(N-2 downto 0);
+				--R(15)<=R(15) xor R(14);
+				--R(2)<=R(1) xor R(15);
+				--R(0)<= R(15) xor M(COUNT);
+
+
+				R(7)<=R(7) xor R(6);
+				R(6)<=R(7) xor R(5);
+				R(5)<=R(7) xor R(4);
+				R(4)<=R(7) xor R(3);
+				R(3)<=R(7) xor R(2);
+				R(2)<=R(1);
+				R(1)<=R(7) xor R(0);
+				R(0)<= R(7) xor M(COUNT);
+
+
 				--R(0)<= R(15) xor INPUT;
 				COUNT<=COUNT+1;
 			end if;
 			if st=st2 then
-				M(MAX-16 to MAX-1)<=R;
+				M(MAX-N to MAX-1)<=R;
 				COUNT<=0;
 				M(MAX-4)<= not M(MAX-4);
-				R<=X"0000";
+				R<=X"00";
 			end if;
 
 		end if;
